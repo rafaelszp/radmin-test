af9c2aeb26e3ce98e4a94e059a9dd43c88c85d23
Rafael Pereira <rafaelszp@users.noreply.github.com>
2019-08-21 14:41:41 -0300

    live preview

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # radmin-test
 
-A test project integrating react-admin and  easy-peasy providing theming and internationalization features
-
+A test project integrating react-admin and easy-peasy providing theming and internationalization features
 
 ## Running in dev mode
+
 ```bash
 yarn start
-``` 
+```
 
 ## Easy-peasy
 
 See:
+
 1. Dispatch example: [Dashboard.js](./src/components/Dashboard.js) file. Lines #76 and #84
 2. Configuration with easy-peasy and reactotron: [store.js](./src/store.js)
 
 ## Theming and Internationalization
 
 See: [App.js](./src/App.js)
+
+## Live Preview
+
+You can live preview this project following this link: [](https://codesandbox.io/s/radmin-test-v0gef)
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,124 +1,138 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState, Fragment } from "react";
 import {
-    SimpleForm,
-    Edit,
-    TextInput,
-    ReferenceInput,
-    SelectInput,
-    DisabledInput,
-    LongTextInput,
-    Datagrid,
-    Create,
-    ReferenceField,
-    TextField,
-    List,
-    Filter,
-    SimpleList,
-    Responsive,
-    translate,
-} from 'react-admin'
+  SimpleForm,
+  Edit,
+  TextInput,
+  ReferenceInput,
+  SelectInput,
+  DisabledInput,
+  LongTextInput,
+  Datagrid,
+  Create,
+  ReferenceField,
+  TextField,
+  List,
+  Filter,
+  SimpleList,
+  Responsive,
+  translate
+} from "react-admin";
 
 import {
-    required,
-    minLength,
-    maxLength,
-    minValue,
-    maxValue,
-    number,
-    regex,
-    email,
-    choices
-} from 'react-admin'
+  required,
+  minLength,
+  maxLength,
+  minValue,
+  maxValue,
+  number,
+  regex,
+  email,
+  choices
+} from "react-admin";
 
+import moment from "moment";
+import "moment/locale/pt-br";
 
-
-import moment from 'moment'
-import 'moment/locale/pt-br';
-
-
-import RichTextInput from 'ra-input-rich-text';
+import RichTextInput from "ra-input-rich-text";
 
 const PostTitle = ({ record }) => {
-    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+  return <span>Post {record ? `"${record.title}"` : ""}</span>;
 };
 
 export const PostEdit = props => (
-    <Edit title={<PostTitle />} {...props}>
-        <SimpleForm>
-            <DisabledInput source="id" />
-            <ReferenceInput source="userId" reference="users">
-                <SelectInput optionText="name" />
-            </ReferenceInput>
-            <TextInput source="title" />
-            <RichTextInput source="body" />
-        </SimpleForm>
-    </Edit>
+  <Edit title={<PostTitle />} {...props}>
+    <SimpleForm>
+      <DisabledInput source="id" />
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <RichTextInput source="body" />
+    </SimpleForm>
+  </Edit>
 );
 
-export const PostCreate = translate(({translate,...props}) => {
-
-    const validateTitle = [required(), minLength(3), maxLength(15)];
-    const validateBody = [required(), minLength(30), maxLength(100)];
-    const validateUser = [
-        number(translate('ra.validation.selectFieldError', { name: translate('ra.auth.username') })),
-        required(),
-        minValue(1, translate('ra.validation.selectFieldError', { name: translate('ra.auth.username') }))
-    ]
-
-    const initialPost = {
-        title: '',
-        body: '',
-        userId: 0
-    }
-
-    return (
-        <Fragment>
-            <Create {...props}>
-                <SimpleForm defaultValue={initialPost}>
-                    <ReferenceInput label="Usuário" source="userId" reference="users" validate={validateUser}>
-                        <SelectInput optionText="name" />
-                    </ReferenceInput>
-                    <TextInput source="title" validate={validateTitle} />
-                    <LongTextInput source="body" validate={validateBody} />
-                </SimpleForm>
-            </Create>
-        </Fragment>
-
-    );
-})
-
-export const PostList = translate(({translate,...props}) => {
-    return (
-        <List filters={<PostFilter />} title={translate('app.label.post')}  {...props}>
-            <Responsive
-                small={
-                    <SimpleList
-                        primaryText={record => record.title}
-                        secondaryText={record => `${record.views} views`}
-                        tertiaryText={record => moment().format('DD/MM/YYYY', record.published_at)}
-                    />
-                }
-                medium={
-                    <Datagrid rowClick="edit">
-                        <ReferenceField source="userId" reference="users">
-                            <TextField source="id" />
-                        </ReferenceField>
-                        <TextField source="id" />
-                        <TextField source="title" />
-                        <TextField source="body" />
-                    </Datagrid>
-
-                }
-            />
-        </List>
+export const PostCreate = translate(({ translate, ...props }) => {
+  const validateTitle = [required(), minLength(3), maxLength(15)];
+  const validateBody = [required(), minLength(30), maxLength(100)];
+  const validateUser = [
+    number(
+      translate("ra.validation.selectFieldError", {
+        name: translate("ra.auth.username")
+      })
+    ),
+    required(),
+    minValue(
+      1,
+      translate("ra.validation.selectFieldError", {
+        name: translate("ra.auth.username")
+      })
     )
+  ];
+
+  const initialPost = {
+    title: "",
+    body: "",
+    userId: 0
+  };
+
+  return (
+    <Fragment>
+      <Create {...props}>
+        <SimpleForm defaultValue={initialPost}>
+          <ReferenceInput
+            label="Usuário"
+            source="userId"
+            reference="users"
+            validate={validateUser}
+          >
+            <SelectInput optionText="name" />
+          </ReferenceInput>
+          <TextInput source="title" validate={validateTitle} />
+          <LongTextInput source="body" validate={validateBody} />
+        </SimpleForm>
+      </Create>
+    </Fragment>
+  );
 });
 
-const PostFilter = (props) => (
-    <Filter {...props}>
-        <TextInput label="app.label.search" source="q" alwaysOn />
-        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
-            <SelectInput optionText="name" />
-        </ReferenceInput>
-    </Filter>
+export const PostList = translate(({ translate, ...props }) => {
+  return (
+    <List
+      filters={<PostFilter />}
+      title={translate("app.label.post")}
+      {...props}
+    >
+      <Responsive
+        small={
+          <SimpleList
+            primaryText={record => record.title}
+            secondaryText={record => `${record.views} views`}
+            tertiaryText={record =>
+              moment().format("DD/MM/YYYY", record.published_at)
+            }
+          />
+        }
+        medium={
+          <Datagrid rowClick="edit">
+            <ReferenceField source="userId" reference="users">
+              <TextField source="id" />
+            </ReferenceField>
+            <TextField source="id" />
+            <TextField source="title" />
+            <TextField source="body" />
+          </Datagrid>
+        }
+      />
+    </List>
+  );
+});
+
+const PostFilter = props => (
+  <Filter {...props}>
+    <TextInput label="app.label.search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>
+  </Filter>
 );
