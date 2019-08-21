e8927dbadb4f8d097331e56e3e7e18f4a68165e3
Rafael Pereira <rafaelszp@users.noreply.github.com>
2019-08-21 14:25:11 -0300

    changed provider to https
    changed provider to https

--- src/App.js
+++ src/App.js
@@ -1,52 +1,48 @@
-import React from 'react';
-import { Admin, Resource, resolveBrowserLocale,translate } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import React from "react";
+import { Admin, Resource, resolveBrowserLocale, translate } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
 
-import { Provider } from 'react-redux';
-import { StoreProvider } from 'easy-peasy';
-import createAdminStore from './store';
-import { createHashHistory } from 'history';
+import { Provider } from "react-redux";
+import { StoreProvider } from "easy-peasy";
+import createAdminStore from "./store";
+import { createHashHistory } from "history";
 
-import portugueseMessages from 'ra-language-portuguese';
-import * as domainMessages from './components/appMessages'
-import englishMessages from 'ra-language-english';
+import portugueseMessages from "ra-language-portuguese";
+import * as domainMessages from "./components/appMessages";
+import englishMessages from "ra-language-english";
 
-import { createMuiTheme } from '@material-ui/core/styles';
-import indigo from '@material-ui/core/colors/indigo';
-import red from '@material-ui/core/colors/red';
-import yellow from '@material-ui/core/colors/yellow';
+import { createMuiTheme } from "@material-ui/core/styles";
+import indigo from "@material-ui/core/colors/indigo";
+import red from "@material-ui/core/colors/red";
+import yellow from "@material-ui/core/colors/yellow";
 
+import Dashboard from "./components/Dashboard";
+import NotFound from "./components/NotFound";
+import UserList from "./components/UserList";
+import { PostList, PostEdit, PostCreate } from "./components/Post";
 
-
-import Dashboard from './components/Dashboard'
-import NotFound from './components/NotFound'
-import UserList from './components/UserList'
-import { PostList, PostEdit, PostCreate } from './components/Post'
-
-
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const history = createHashHistory();
 
 //https://v1.material-ui.com/style/color/
 const theme = createMuiTheme({
   palette: {
-    
     primary: indigo,
     secondary: {
-      light: '#00A78D',
-      main: '#00A78D',
-      contrastText: '#fff',
+      light: "#00A78D",
+      main: "#00A78D",
+      contrastText: "#fff"
     },
     error: {
       ...red,
       light: yellow.light
     },
     contrastThreshold: 3,
-    tonalOffset: 0.2,
+    tonalOffset: 0.2
   },
   overrides: {
     MuiSnackbarContent: {
@@ -54,33 +50,28 @@ const theme = createMuiTheme({
         backgroundColor: "#00A78D"
       }
     }
-  },
+  }
 });
 
-
-
-const browserLocale = resolveBrowserLocale() || 'pt';
+const browserLocale = resolveBrowserLocale() || "pt";
 const messages = {
-  pt: {...portugueseMessages,...domainMessages.pt},
-  en: {...englishMessages,...domainMessages.en},
-}
+  pt: { ...portugueseMessages, ...domainMessages.pt },
+  en: { ...englishMessages, ...domainMessages.en }
+};
 const i18nProvider = () => {
-  
-  return messages[browserLocale]
+  return messages[browserLocale];
 };
 
 const authProvider = () => Promise.resolve();
 
-
-const store=createAdminStore({
+const store = createAdminStore({
   authProvider,
   dataProvider,
   i18nProvider,
-  history,
+  history
 });
 
-
-const App = ({translate}) => (
+const App = ({ translate }) => (
   <Provider store={store}>
     <StoreProvider store={store}>
       <Admin
@@ -90,12 +81,25 @@ const App = ({translate}) => (
         catchAll={NotFound}
         history={history}
         locale={browserLocale}
-        i18nProvider={i18nProvider}>
-        <Resource name="users" options={{label:'app.label.user'}} list={UserList} icon={UserIcon} />
-        <Resource name="posts" options={{label:'app.label.post'}} list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        i18nProvider={i18nProvider}
+      >
+        <Resource
+          name="users"
+          options={{ label: "app.label.user" }}
+          list={UserList}
+          icon={UserIcon}
+        />
+        <Resource
+          name="posts"
+          options={{ label: "app.label.post" }}
+          list={PostList}
+          edit={PostEdit}
+          create={PostCreate}
+          icon={PostIcon}
+        />
       </Admin>
     </StoreProvider>
-  </Provider >
-)
+  </Provider>
+);
 
 export default translate(App);
